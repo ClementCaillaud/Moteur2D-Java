abc094565d23c2f68c98fec5d7683cfa5000833c
Clément Caillaud <clement.caillaud97@gmail.com>
2019-06-13 17:01:10 +0200

    Ajouter une image

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,17 +1,22 @@
 package demo;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import moteur.Direction;
 import moteur.Fenetre;
 import moteur.Scene;
 import moteur.objets.Cercle;
+import moteur.objets.Sprite;
 import moteur.objets.Texte;
 
 public class Demo 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Lancement de la démo...");
@@ -19,11 +24,13 @@ public class Demo
 		Cercle c = new Cercle("C1", 50, 100, Color.BLUE, 50, 50);
 		Cercle c2 = new Cercle("C2", 100, 50, Color.RED, 10, 80);
 		Texte texte = new Texte("FPS", 10, 20, Color.BLACK, "0 FPS");
+		Sprite s = new Sprite("I1", 200, 200, 50, 50, ImageIO.read(new File("img_test.png")));
 		
 		Scene scene = new Scene();
 		scene.ajouter_game_object(c);
 		scene.ajouter_game_object(c2);
 		scene.ajouter_game_object(texte);
+		scene.ajouter_game_object(s);
 		
 		Fenetre fenetre = new Fenetre(600, 600, "Démo moteur 2D");
 		fenetre.charger_scene(scene);
--- src/moteur/objets/Sprite.java
+++ src/moteur/objets/Sprite.java
@@ -1,13 +1,32 @@
 package moteur.objets;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
 public class Sprite extends GameObject
 {
-	
+	private Image image;
+	private int tailleX;
+	private int tailleY;
 
-	public Sprite(String nom, int positionX, int positionY) 
+	public Sprite(String nom, int positionX, int positionY, Image image) 
 	{
 		super(nom, positionX, positionY);
-		// TODO Auto-generated constructor stub
+		this.image = image;
+	}
+	
+	public Sprite(String nom, int positionX, int positionY, int tailleX, int tailleY, Image image) 
+	{
+		super(nom, positionX, positionY);
+		this.image = image;
+		this.tailleX = tailleX;
+		this.tailleY = tailleY;
+	}
+	
+	@Override
+	public void dessiner(Graphics g)
+	{
+		g.drawImage(image, positionX, positionY, tailleX, tailleY, null);
 	}
 
 }
